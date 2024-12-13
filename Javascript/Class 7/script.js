2942c69edfdf9dea7db4f285760a60d75ae2868b
ManojNambiraj <postboxmano@gmail.com>
2024-12-13 20:43:45 +0530

    JS DOM and Bootstrap

---
+++ Javascript/Class 7/script.js
@@ -0,0 +1,37 @@
+// DOM
+
+// let your_account_balance = 0;
+// let amt;
+
+// let ele = document.getElementById("demo");
+
+// const handleChange = (event) => {
+//   amt = Number(event.target.value);
+// };
+
+// const handleDeposite = () => {
+//   your_account_balance += amt;
+
+//   ele.innerText = your_account_balance;
+// };
+
+// const handleWithdraw = () => {
+//   your_account_balance -= amt;
+
+//   ele.innerText = your_account_balance;
+// };
+
+
+// let newEle = document.createElement("div")
+
+// newEle.setAttribute("class", "firstHead")
+
+// newEle.style.backgroundColor = "Yellow"
+// newEle.style.color = "red"
+
+// let h1Ele = document.createElement("h1")
+
+// h1Ele.innerText = "Hello world"
+
+// document.body.append(newEle);
+// newEle.append(h1Ele)
